7958d2f4a70af22846f282050b7361d899680d4c
Emily Barnes <33987921+emlbarnes@users.noreply.github.com>
2021-10-11 17:31:25 +0800

    doc part 3 unfinished
    written more views. detail results and vote. added alumni. added sponsors. added stats. unable to finally configure but placeholder views set.
    completed as per tutorial generic voting including "polls" vote system, this function is working.  estimate 2/3 of documentation part 3 is now complete- up to but not including Rasininga 404 error title.

--- groupproject/swc/urls.py
+++ groupproject/swc/urls.py
@@ -3,4 +3,12 @@ from django.urls.resolvers import URLPattern
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('alumni/', views.alumni, name='alumni'),
+    path('sponsors/', views.sponsors, name='sponsors'),
+    path('stats/', views.stats, name='stats'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+
