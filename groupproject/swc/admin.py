f5d199ff26eb3749c58af6de870d5b850a33faa1
rachelBurford <rachel@pilbaratax.com.au>
2021-10-16 12:35:35 +0800

    Update admin.py
    Locations

--- groupproject/swc/admin.py
+++ groupproject/swc/admin.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from swc.models import Location, Language, Course, Participant, Schedule, Sponsors
 
 class LocationAdmin(admin.ModelAdmin):
-    fields = ['region', 'state']
+    list_display =('state','region','long','lat')
 
 
-admin.site.register(Location)
 admin.site.register(Language)
 admin.site.register(Course)
 admin.site.register(Participant)
 admin.site.register(Schedule)
 admin.site.register(Sponsors)
+admin.site.register(Location,LocationAdmin)
+
 
 
 
