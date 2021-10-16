34a3feafb45d9a0eb91be88c81d27dcfb9e8780f
rachelBurford <rachel@pilbaratax.com.au>
2021-10-16 10:52:59 +0800

    Update Models
    Model Update Gender

--- groupproject/swc/models.py
+++ groupproject/swc/models.py
@@ -22,12 +22,18 @@ class Course(models.Model):
     def __str__(self):
         return self.program + " - " + self.location.region
 
+GENDER_CHOICES = [
+('F','Female'),
+('M','Male'),
+('N','Non-Binary'),
+('O','Other'),
+]
 
 class Participant(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
-    gender = models.CharField(max_length=30)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     home_location = models.CharField(max_length=100)
     phone = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
@@ -37,7 +43,7 @@ class Participant(models.Model):
     in_mentor = models.CharField(max_length=30)  
     language = models.ManyToManyField(Language)
     def __str__(self):
-        return self.first_name + " "+ self.last_name
+        return self.first_name + " "+ self.last_name + self.gender
 
 class Schedule(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
