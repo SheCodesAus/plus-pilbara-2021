955a5c27070941f02c62bb5c1ef18a71b6a036e3
Dtaylor709 <84775234+Dtaylor709@users.noreply.github.com>
2021-10-18 17:37:52 +0800

    changed setView to Australia

--- groupproject/swc/static/swc/map-scripts.js
+++ groupproject/swc/static/swc/map-scripts.js
@@ -1,7 +1,7 @@
 console.log(statedata)
 const drawMap = function() {
 
-  var map = L.map('map').setView([37.8, -96], 4);
+  var map = L.map('map').setView([-27.833, 133.583], 4);
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
