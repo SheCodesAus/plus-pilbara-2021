fa51b1a4a85e9da44ef9dc59ede8243a628c8498
rachelBurford <rachel@pilbaratax.com.au>
2021-10-19 18:38:35 +0800

    Alumni page
    added content

--- groupproject/swc/urls.py
+++ groupproject/swc/urls.py
@@ -8,15 +8,10 @@ app_name = 'swc'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('participant/', views.AlumniView.as_view(), name='partipant'),
+    path('alumni/', views.AlumniView.as_view(), name='participant'),
     path('sponsors/', views.sponsors, name='sponsors'),
     path('pathways/', views.pathways, name='pathways'),
     path('stats/', views.stats, name='stats'),
-    # path('add-location/', views.AddLocationView.as_view(), name='locationView'),
-    path('<int:pk>/', views.AlumniView.as_view(), name='story'),
-    # path('<int:question_id>/', views.detail, name='detail'),
-    # path('<int:question_id>/results/', views.results, name='results'),
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
     path('add-participant/', views.AddParticipantView.as_view(), name='participantView')
     
 ]
