e74f0c19287d094694d10915ad63bb51a5a76530
rachelBurford <rachel@pilbaratax.com.au>
2021-10-20 20:55:15 +0800

    updates
    Javascript linked to page
    basic ugly layout set up - grid for mai hero + text
    Next steps - database calling and python to get numbers from database

--- groupproject/swc/views.py
+++ groupproject/swc/views.py
@@ -4,21 +4,16 @@ from django.urls import reverse_lazy
 from .models import Location, Language, Course, Participant, Schedule, Sponsors
 from .forms import ParticipantForm
 
-def index(request):
-    return render(request, 'swc/index.html')
+
+class IndexView(generic.ListView):
+    template_name = 'swc/index.html'
+    model = Participant
 
 
-def pathways (request):
-    return HttpResponse("You're looking at question.")
-
-def stats (request):
-    return render(request, 'swc/index.html')
-
-# class AddLocationView(Generic.CreateView):
-#     form_class = LocationForm
-#     context_object_name = 'locationForm'
-#     template_name = 'swc/index.html'
-#     success_url = reverse_lazy('swc:index')
+class PathwayView(generic.ListView):
+    model = Course
+    template_name = 'swc/pathways.html'
+    context_object_name = 'pathways'
 
 class AlumniView(generic.ListView):
     model = Participant
@@ -36,3 +31,8 @@ class SponsorView(generic.ListView):
     model = Sponsors
     template_name = 'swc/sponsors.html'
     context_object_name = 'sponsors'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['number'] = 100
+        return context
