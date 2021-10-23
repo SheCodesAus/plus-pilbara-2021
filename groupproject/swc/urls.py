7987d2e9191250a8ec3d0beb4c67723cfc86f4d7
jbehrend89 <84775195+jbehrend89@users.noreply.github.com>
2021-10-23 12:05:14 +0800

    update URLs pathways

--- groupproject/swc/urls.py
+++ groupproject/swc/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('alumni/', views.AlumniView.as_view(), name='participant'),
     path('sponsors/', views.SponsorView.as_view(), name='sponsors'),
-    path('pathways/', views.PathwayView.as_view, name='pathways'),
+    path('pathways/', views.PathwayView.as_view(), name='pathways'),
     path('add-participant/', views.AddParticipantView.as_view(), name='participantView'),
     path('participant-interview/', views.ParticipantInterviewView.as_view(), name='participantInterview')
     
