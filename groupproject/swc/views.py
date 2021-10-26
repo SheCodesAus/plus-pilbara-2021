49b1135a9cdf264822384431d95722d4a9112a95
Dtaylor709 <84775234+Dtaylor709@users.noreply.github.com>
2021-10-27 00:18:52 +0800

    filter data by course_location_state

--- groupproject/swc/views.py
+++ groupproject/swc/views.py
@@ -10,26 +10,135 @@ class IndexView(generic.ListView):
     template_name = 'swc/index.html'
     model = Participant
 
-    
+
     def get_context_data(self, **kwargs):
-        context= super().get_context_data(**kwargs)
-        Shecode_statedata =  {
-        "Western Australia": 1500,
-        "Northern Territory": 0,
-        "South Australia": 150,
-        "Queensland": 600,
-        "New South Wales": 40,
-        "Australian Capital Territory":60,
-        "Victoria":250,
-        "Tasmania": 30,
-        
+        context = super().get_context_data(**kwargs)
+        # context['Australia_participants_total'] = Participant.objects.all().count()
+        # context['state_participants_total'] = Participant.objects.filter(state="").count()
 
+        # context['Australia_participants_by_course'] = Participant.objects.all().filter(Course).count()
+        # context['state_participants_by_course'] = Participant.objects.filter(course__state="").count()
+
+        # context['Australia_metro_participants_total'] = Participant.objects.all().filter(course__state__region="Perth"+"Adelaide"+"Darwin"+ "Brisbane"+"Sydney"+"Melbourne"+"Hobart"+"Canberra").count()
+        # context['state_metro_participants_total'] = Participant.objects.filter(course__state="").count()
+
+        # context['Australia_regional_participants_total'] = Participant.objects.exclude(course__location__region="").count()
+        # context['state_regional_participants_total'] = Participant.objects.filter(course__state="").count()
+    
+
+        # hayley was here
+        ## show total number = statedata.wa + state names?
+        ## number by course
+        ## number by state
+
+        shecodes_statedata = { 
+            'WA': {
+                'regional': {
+                    '1 day': Participant.objects.all().filter(course__program="Workshop", course__location__state="WA").exclude(course__location__region__contains="Perth").count(), # ....
+                    'flash': Participant.objects.all().filter(course__program="Flash", course__location__state="WA").exclude(course__location__region__contains="Perth").count(), # ....
+                    'plus': Participant.objects.all().filter(course__program="Plus", course__location__state="WA").exclude(course__location__region__contains="Perth").count(), # ....
+                    'alumni_mentor': Participant.objects.all().filter(in_mentor="yes", course__location__state="WA").exclude(course__location__region__contains="Perth").count(),
+                },
+                'metro': {
+                    '1 day': Participant.objects.all().filter(course__program="Workshop", course__location__state="WA", course__location__region__contains="Perth").count(), # ....
+                    'flash': Participant.objects.all().filter(course__program="Flash", course__location__state="WA", course__location__region__contains="Perth").count(), # .... statedata['wa'][metro][flash], statedata.wa.metro.flash
+                    'plus': Participant.objects.all().filter(course__program="Plus", course__location__state="WA", course__location__region__contains="Perth").count(),
+                    'alumni_mentor': Participant.objects.all().filter(in_mentor="yes", course__location__state="WA", course__location__region__contains="Perth").count(), # ....
+                },
+            },
+            'SA': {
+                'regional': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # ....
+                    'plus': Participant.objects.all(), # ....
+                },
+                'metro': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # .... statedata['wa'][metro][flash], statedata.wa.metro.flash
+                    'plus': Participant.objects.all(), # ....
+                },
+            },
+            'NT': {
+                'regional': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # ....
+                    'plus': Participant.objects.all(), # ....
+                },
+                'metro': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # .... statedata['wa'][metro][flash], statedata.wa.metro.flash
+                    'plus': Participant.objects.all(), # ....
+                },
+            },
+            'QLD': {
+                'regional': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # ....
+                    'plus': Participant.objects.all(), # ....
+                },
+                'metro': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # .... statedata['wa'][metro][flash], statedata.wa.metro.flash
+                    'plus': Participant.objects.all(), # ....
+                },
+            },
+            'NSW': {
+                'regional': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # ....
+                    'plus': Participant.objects.all(), # ....
+                },
+                'metro': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # .... statedata['wa'][metro][flash], statedata.wa.metro.flash
+                    'plus': Participant.objects.all(), # ....
+                },
+            },
+            'VIC': {
+                'regional': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # ....
+                    'plus': Participant.objects.all(), # ....
+                },
+                'metro': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # .... statedata['wa'][metro][flash], statedata.wa.metro.flash
+                    'plus': Participant.objects.all(), # ....
+                },
+            },
+            'ACT': {
+                'regional': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # ....
+                    'plus': Participant.objects.all(), # ....
+                },
+                'metro': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # .... statedata['wa'][metro][flash], statedata.wa.metro.flash
+                    'plus': Participant.objects.all(), # ....
+                },
+            },
+            'TAS': {
+                'regional': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # ....
+                    'plus': Participant.objects.all(), # ....
+                },
+                'metro': {
+                    '1 day': Participant.objects.all(), # ....
+                    'flash': Participant.objects.all(), # .... statedata['wa'][metro][flash], statedata.wa.metro.flash
+                    'plus': Participant.objects.all(), # ....
+                },
+            },
         }
-        json_data = json.dumps(Shecode_statedata)
+        print(shecodes_statedata['WA']['regional']['1 day'])
+        json_data = json.dumps(shecodes_statedata)
+        context = { "jsonData": json_data }
 
-        context["jsonData"]=json_data
-
-
+        return context 
+        # if someone clicked "1 day"
+        # add wa 1 day + qld 1 day + nsw + 1 day....
+  
 
 class PathwayView(generic.ListView):
     model = Course
