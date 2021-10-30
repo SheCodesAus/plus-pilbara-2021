5e7a05df9a4589ae83019baa08e72a5a87b149fd
runda-git <84775206+runda-git@users.noreply.github.com>
2021-10-30 09:46:50 +0800

    footer and header

--- groupproject/swc/urls.py
+++ groupproject/swc/urls.py
@@ -8,13 +8,12 @@ app_name = 'swc'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('alumni/', views.AlumniView.as_view(), name='alumni'),
+    path('alumni/', views.AlumniView.as_view(), name='participant'),
     path('sponsors/', views.SponsorView.as_view(), name='sponsors'),
     path('pathways/', views.PathwayView.as_view(), name='pathways'),
     path('add-participant/', views.AddParticipantView.as_view(), name='participantView'),
     path('participant-interview/', views.ParticipantInterviewView.as_view(), name='participantInterview'),
     path('enrol-student/', views.StudentEnrollmentView.as_view(), name='StudentEnrollmentsForm'),
-
 ]
 
 
