2dbe9faa3966343a34c3b1927f36f44c16e9f2a1
rachelBurford <rachel@pilbaratax.com.au>
2021-10-30 10:03:42 +0800

    Update settings.py
    update

--- groupproject/groupproject/settings.py
+++ groupproject/groupproject/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get
-    ('DJANGO_SECRET_KEY',
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
     '5*15pt5log&-bjpkqo0117!b!x4do-mgmxvg8n$3016384zz(7'
 )
 # SECURITY WARNING: don't run with debug turned on in production!
