896b7988439ab2dd3d1279bc4304c7a899e7709d
rachelBurford <rachel@pilbaratax.com.au>
2021-11-02 14:29:32 +0800

    Slider

--- groupproject/swc/static/swc/slides.js
+++ groupproject/swc/static/swc/slides.js
@@ -1,12 +1,17 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+document.addEventListener('DOMContentLoaded', (event) => {
+  showSlides(slideIndex)
+  setInterval(() => { 
+    increase()
+   }, 1000)
+});
 
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function increase() {
+function increase(n=1) {
   showSlides(slideIndex += n); 
 }
 
@@ -19,17 +24,17 @@ function currentSlide(m) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  // var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  // for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(" active", "");
+  // }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  // dots[slideIndex-1].className += " active";
 }
 
 function onError() {
--- groupproject/swc/static/swc/sponsors.js
+++ groupproject/swc/static/swc/sponsors.js
@@ -1,23 +1,5 @@
 window.onload = function () {
     drawMap()
-    // create new elements
-    let speechBubbleElement = document.createElement("div");
-    speechBubbleElement.classList.add("speech-bubble");
-    let messageBox = document.createElement("p");
-    messageBox.innerText = "...";
-    messageBox.id = "message-box";
-    speechBubbleElement.appendChild(messageBox);
-
-    // add new element to page
-    let outputElement = document.getElementById("output");
-    outputElement.appendChild(speechBubbleElement);
-};
-
-function showMessage() {
-    let message = document.getElementById("message-text").value;
-    let name = document.getElementById("name").value;
-    let messageBox = document.getElementById("message-box");
-    messageBox.innerText = message + " - " + name;
 };
 
 
